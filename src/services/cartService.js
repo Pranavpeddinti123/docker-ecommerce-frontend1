0b672f76da9c75ea82e1ac77e1c8b6ab1819dad3
Pranavpeddinti123 <163733336+Pranavpeddinti123@users.noreply.github.com>
2025-09-15 10:05:33 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/cart";
+const API_URL = "http://ec2-3-80-118-11.compute-1.amazonaws.com:8081/api/cart";
 
 const API_BASE = "http://localhost:8081/api/cart";
 
